108681a8865b6b1b4824faef0218ee16647c35e5
abdelboulgha <118727738+abdelboulgha@users.noreply.github.com>
2025-10-23 17:34:22 +0100

    exercice1

--- src/main/java/ma/projet/exercice1/presentation/TestExercice1.java
+++ src/main/java/ma/projet/exercice1/presentation/TestExercice1.java
@@ -6,10 +6,6 @@ import ma.projet.exercice1.classes.LigneCommande;
 import ma.projet.exercice1.classes.Produit;
 import ma.projet.exercice1.dao.IDao;
 import ma.projet.exercice1.dao.IProduitDao;
-import ma.projet.exercice1.service.CategorieService;
-import ma.projet.exercice1.service.CommandeService;
-import ma.projet.exercice1.service.LigneCommandeService;
-import ma.projet.exercice1.service.ProduitService;
 import ma.projet.exercice1.util.HibernateUtil;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -18,109 +14,113 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 
 public class TestExercice1 {
+
     public static void main(String[] args) throws Exception {
-        ApplicationContext context = new AnnotationConfigApplicationContext(HibernateUtil.class);
+        // Initialisation du contexte Spring
+        ApplicationContext springContext = new AnnotationConfigApplicationContext(HibernateUtil.class);
 
-        // Utiliser les noms de beans corrects
-        IProduitDao produitDao = context.getBean("produitService", IProduitDao.class);
-        IDao<Categorie> categorieDao = context.getBean("categorieService", IDao.class);
-        IDao<Commande> commandeDao = context.getBean("commandeService", IDao.class);
-        IDao<LigneCommande> ligneDao = context.getBean("ligneCommandeService", IDao.class);
+        // Récupération des services via l'injection de dépendances
+        IProduitDao serviceProduit = springContext.getBean("produitService", IProduitDao.class);
+        IDao<Categorie> serviceCategorie = springContext.getBean("categorieService", IDao.class);
+        IDao<Commande> serviceCommande = springContext.getBean("commandeService", IDao.class);
+        IDao<LigneCommande> serviceLigneCommande = springContext.getBean("ligneCommandeService", IDao.class);
 
-        // ----- Add Categories -----
-        Categorie cat1 = new Categorie();
-        cat1.setCode("ELEC123");
-        cat1.setLibelle("Électronique");
-        categorieDao.create(cat1);
+        // === Création des catégories ===
+        Categorie categorieElectronique = new Categorie();
+        categorieElectronique.setCode("ELEC123");
+        categorieElectronique.setLibelle("Électronique");
+        serviceCategorie.create(categorieElectronique);
 
-        Categorie cat2 = new Categorie();
-        cat2.setCode("BOOK456");
-        cat2.setLibelle("Livres");
-        categorieDao.create(cat2);
+        Categorie categorieLivres = new Categorie();
+        categorieLivres.setCode("BOOK456");
+        categorieLivres.setLibelle("Livres");
+        serviceCategorie.create(categorieLivres);
 
-        // ----- Produits -----
-        Produit p1 = new Produit();
-        p1.setReference("Smartphone");
-        p1.setPrix(500.0);
-        p1.setCategorie(cat1);
-        produitDao.create(p1);
+        // === Création des produits ===
+        Produit smartphone = new Produit();
+        smartphone.setReference("Smartphone");
+        smartphone.setPrix(500.0);
+        smartphone.setCategorie(categorieElectronique);
+        serviceProduit.create(smartphone);
 
-        Produit p2 = new Produit();
-        p2.setReference("Laptop");
-        p2.setPrix(1200.0);
-        p2.setCategorie(cat1);
-        produitDao.create(p2);
+        Produit laptop = new Produit();
+        laptop.setReference("Laptop");
+        laptop.setPrix(1200.0);
+        laptop.setCategorie(categorieElectronique);
+        serviceProduit.create(laptop);
 
-        Produit p3 = new Produit();
-        p3.setReference("Java Book");
-        p3.setPrix(150.0);
-        p3.setCategorie(cat2);
-        produitDao.create(p3);
+        Produit livreJava = new Produit();
+        livreJava.setReference("Java Book");
+        livreJava.setPrix(150.0);
+        livreJava.setCategorie(categorieLivres);
+        serviceProduit.create(livreJava);
 
-        // ----- Commandes -----
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        Commande cmd1 = new Commande();
-        cmd1.setDate(sdf.parse("2025-10-10"));
-        commandeDao.create(cmd1);
+        // === Création des commandes ===
+        SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
+        Commande commande1 = new Commande();
+        commande1.setDate(formatDate.parse("2025-10-10"));
+        serviceCommande.create(commande1);
 
-        Commande cmd2 = new Commande();
-        cmd2.setDate(sdf.parse("2025-10-15"));
-        commandeDao.create(cmd2);
+        Commande commande2 = new Commande();
+        commande2.setDate(formatDate.parse("2025-10-15"));
+        serviceCommande.create(commande2);
 
-        // ----- Lignes -----
-        LigneCommande ligne1 = new LigneCommande();
-        ligne1.setCommande(cmd1);
-        ligne1.setProduit(p1);
-        ligne1.setQuantite(7);
-        ligneDao.create(ligne1);
+        // === Création des lignes de commande ===
+        LigneCommande ligneCommande1 = new LigneCommande();
+        ligneCommande1.setCommande(commande1);
+        ligneCommande1.setProduit(smartphone);
+        ligneCommande1.setQuantite(7);
+        serviceLigneCommande.create(ligneCommande1);
 
-        LigneCommande ligne2 = new LigneCommande();
-        ligne2.setCommande(cmd1);
-        ligne2.setProduit(p2);
-        ligne2.setQuantite(11);
-        ligneDao.create(ligne2);
+        LigneCommande ligneCommande2 = new LigneCommande();
+        ligneCommande2.setCommande(commande1);
+        ligneCommande2.setProduit(laptop);
+        ligneCommande2.setQuantite(11);
+        serviceLigneCommande.create(ligneCommande2);
 
-        LigneCommande ligne3 = new LigneCommande();
-        ligne3.setCommande(cmd1);
-        ligne3.setProduit(p3);
-        ligne3.setQuantite(2);
-        ligneDao.create(ligne3);
+        LigneCommande ligneCommande3 = new LigneCommande();
+        ligneCommande3.setCommande(commande1);
+        ligneCommande3.setProduit(livreJava);
+        ligneCommande3.setQuantite(2);
+        serviceLigneCommande.create(ligneCommande3);
 
-        // ----- Test -----
-        LigneCommande l = ligneDao.findById(2);
-        System.out.println("Commande : " + l.getCommande().getId() + "\t Date: " +  l.getCommande().getDate());
-        System.out.println("List des produits : ");
-        System.out.println("Reference \t Prix \t Quantite");
-        List<Produit> produitsCmd1 = produitDao.findProduitsByCommande(l.getCommande().getId());
-        for (Produit p : produitsCmd1) {
-            System.out.println(p.getReference() + " \t " + p.getPrix() + " \t " + produitDao.findQuantiteOfProduitsInLigne(p.getId()));
+        // === Test d'affichage des produits d'une commande ===
+        LigneCommande ligneTest = serviceLigneCommande.findById(2);
+        System.out.println("Commande : " + ligneTest.getCommande().getId() + "\t Date: " + ligneTest.getCommande().getDate());
+        System.out.println("Liste des produits : ");
+        System.out.println("Référence \t Prix \t Quantité");
+        List<Produit> produitsCommande = serviceProduit.findProduitsByCommande(ligneTest.getCommande().getId());
+        for (Produit produit : produitsCommande) {
+            System.out.println(produit.getReference() + " \t " + produit.getPrix() + " \t " +
+                    serviceProduit.findQuantiteOfProduitsInLigne(produit.getId()));
         }
         System.out.println("\n");
 
-        // ----- Test search methods -----
-        Categorie categorie = categorieDao.findById(1);
-        List<Produit> produitsCat1 = produitDao.findProduitsByCategorie(categorie.getId());
-        System.out.println("\n--- Produits par catégorie " + categorie.getLibelle() + " ---");
-        for (Produit p : produitsCat1) {
-            System.out.println(p.getReference() + " - " + p.getPrix());
+        // === Test des méthodes de recherche ===
+        Categorie categorieTest = serviceCategorie.findById(1);
+        List<Produit> produitsParCategorie = serviceProduit.findProduitsByCategorie(categorieTest.getId());
+        System.out.println("\n--- Produits par catégorie " + categorieTest.getLibelle() + " ---");
+        for (Produit produit : produitsParCategorie) {
+            System.out.println(produit.getReference() + " - " + produit.getPrix());
         }
 
         System.out.println("\n--- Produits dont le prix > 100 ---");
-        List<Produit> produitsPrixSup100 = produitDao.findProduitsPrixSup100();
-        for (Produit p : produitsPrixSup100) {
-            System.out.println(p.getReference() + " - " + p.getPrix());
+        List<Produit> produitsPrixSuperieur100 = serviceProduit.findProduitsPrixSup100();
+        for (Produit produit : produitsPrixSuperieur100) {
+            System.out.println(produit.getReference() + " - " + produit.getPrix());
         }
 
         System.out.println("\n--- Produits commandés entre 2025-10-09 et 2025-10-16 ---");
-        List<Produit> produitsEntreDates = produitDao.findProduitsCommandesEntreDates(sdf.parse("2025-10-09"), sdf.parse("2025-10-16"));
-        for (Produit p : produitsEntreDates) {
-            System.out.println(p.getReference() + " - " + p.getPrix());
+        List<Produit> produitsEntreDates = serviceProduit.findProduitsCommandesEntreDates(
+                formatDate.parse("2025-10-09"), formatDate.parse("2025-10-16"));
+        for (Produit produit : produitsEntreDates) {
+            System.out.println(produit.getReference() + " - " + produit.getPrix());
         }
 
         System.out.println("\n--- Produits de la commande 1 ---");
-        List<Produit> produitsCmd1_2 = produitDao.findProduitsByCommande(commandeDao.findById(1).getId());
-        for (Produit p : produitsCmd1_2) {
-            System.out.println(p.getReference() + " - " + p.getPrix());
+        List<Produit> produitsCommande1 = serviceProduit.findProduitsByCommande(serviceCommande.findById(1).getId());
+        for (Produit produit : produitsCommande1) {
+            System.out.println(produit.getReference() + " - " + produit.getPrix());
         }
     }
 }
